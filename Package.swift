41b1a14a45a64970f4314dd382508fd1249dc616
Jonas Borsch <borschjonas0@gmail.com>
2021-03-18 23:19:35 +0100

    Adjustments in the Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "smol-weather-lib",
+    name: "SmolWeatherLib",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "smol-weather-lib",
+            name: "SmolWeatherLib",
             targets: ["smol-weather-lib"]),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "smol-weather-lib",
+            name: "SmolWeatherLib",
             dependencies: []),
         .testTarget(
             name: "smol-weather-lib-tests",
